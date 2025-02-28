c1876b7ea810e2e596182dbbbb24f6559dc60d50
fikririsyad <fikri.risyad10@gmail.com>
2025-02-28 09:17:31 +0700

    Fix formatting

--- scenes/Player.gd
+++ scenes/Player.gd
@@ -12,6 +12,7 @@ var dashing: bool = false
 var last_left_press_time: float = 0.0
 var last_right_press_time: float = 0.0
 
+
 func _physics_process(delta: float) -> void:
 	velocity.y += delta * gravity
 	handle_jump()
@@ -19,23 +20,25 @@ func _physics_process(delta: float) -> void:
 	handle_walk()
 	move_and_slide()
 
+
 func handle_jump() -> void:
 	if is_on_floor():
-		if Input.is_action_just_pressed('ui_up'):
+		if Input.is_action_just_pressed("ui_up"):
 			velocity.y = jump_speed
 		can_double_jump = true
-	
-	if not is_on_floor() and can_double_jump and Input.is_action_just_pressed('ui_up'):
+
+	if not is_on_floor() and can_double_jump and Input.is_action_just_pressed("ui_up"):
 		velocity.y = jump_speed
 		can_double_jump = false
+
 
 func handle_dash() -> void:
 	if dashing:
 		return
-		
+
 	var current_time = Time.get_ticks_msec() / 1000.0
 	var direction = 0
-	
+
 	if Input.is_action_just_pressed("ui_left"):
 		if current_time - last_left_press_time < dash_press_interval:
 			direction = -1
@@ -45,9 +48,10 @@ func handle_dash() -> void:
 		if current_time - last_right_press_time < dash_press_interval:
 			direction = 1
 		last_right_press_time = current_time
-	
+
 	if direction != 0:
 		start_dash(direction)
+
 
 func start_dash(direction: int) -> void:
 	dashing = true
@@ -57,10 +61,11 @@ func start_dash(direction: int) -> void:
 	await get_tree().create_timer(dash_duration).timeout
 	dashing = false
 
+
 func handle_walk() -> void:
 	if dashing:
 		return
-	
+
 	if Input.is_action_pressed("ui_left"):
 		velocity.x = -walk_speed
 	elif Input.is_action_pressed("ui_right"):
@@ -68,8 +73,10 @@ func handle_walk() -> void:
 	else:
 		velocity.x = 0
 
+
 func _ready() -> void:
 	pass
+
 
 func _process(delta: float) -> void:
 	pass
