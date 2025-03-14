4d0e88e98bb6ece67e32d3e956c35d8f43c34670
fikririsyad <fikri.risyad10@gmail.com>
2025-03-14 20:33:43 +0700

    Fix issues

--- scenes/Player.gd
+++ scenes/Player.gd
@@ -1,22 +1,24 @@
 extends CharacterBody2D
 
-@export var SPEED := 200
-@export var JUMP_SPEED := -600
-@export var GRAVITY := 1200
+@export var speed := 200
+@export var jump_speed := -600
+@export var gravity := 1200
 @onready var animplayer = $AnimatedSprite2D
+
 
 func _get_input():
 	if Input.is_action_just_pressed("ui_up") and is_on_floor():
-		velocity.y = JUMP_SPEED
+		velocity.y = jump_speed
 
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction != 0:
-		velocity.x = direction * SPEED
+		velocity.x = direction * speed
 		animplayer.flip_h = direction < 0
 		animplayer.play("walk right")
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, speed)
 		animplayer.play("idle")
+
 
 func check_collision():
 	for i in get_slide_collision_count():
@@ -27,8 +29,9 @@ func check_collision():
 			var knockback_dir = Vector2(sign(knockback_direction.x), 0)
 			collider.pushed(knockback_dir)
 
+
 func _physics_process(delta: float) -> void:
 	_get_input()
-	velocity.y += GRAVITY * delta
+	velocity.y += gravity * delta
 	move_and_slide()
 	check_collision()
